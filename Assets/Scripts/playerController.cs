d40e17369e7877097281f9555aac0381430129ea
BeauArthurs <BeauArthurs@gmail.com>
2015-02-06 12:57:05 +0100

    Beau_002

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour 
+{
+    private bool jumping;
+    private void Start()
+    {
+        jumping = false;
+    }
+    private void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+    private void Jump()
+    {
+        if (!jumping)
+        {
+            rigidbody.AddForce(new Vector3(0, 2500, 0));
+            jumping = true;
+        }
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Platform")
+        {
+            jumping = false;
+        }
+    }
+}
