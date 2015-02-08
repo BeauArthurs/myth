99737c4992ec7c42bc7cc01a4b6534ce1f5de4b6
BeauArthurs <BeauArthurs@gmail.com>
2015-02-08 19:17:32 +0100

    Beau_004

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class playerController : MonoBehaviour 
 {
+    [SerializeField]
+    private int speed;
+    [SerializeField]
+    private int jumpHight;
+    [SerializeField]
     private bool jumping;
     private void Start()
     {
@@ -10,23 +15,61 @@ public class playerController : MonoBehaviour
     }
     private void Update()
     {
-        
-        if (Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKey(KeyCode.LeftArrow))
         {
-            Jump();
+            transform.position = transform.position + Vector3.left * Time.deltaTime * speed;
         }
-    }
-    private void Jump()
-    {
-        if (!jumping)
+        if (Input.GetKey(KeyCode.RightArrow))
         {
-            rigidbody.AddForce(new Vector3(0, 2500, 0));
-            jumping = true;
+            transform.position = transform.position + Vector3.right * Time.deltaTime * speed;
         }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            if (!jumping)
+            {
+                rigidbody.AddForce(Vector3.up * jumpHight);
+                jumping = true;
+            }
+        }
+        /*for (int a = 0; a < Input.touchCount; a++)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(new Vector3(Input.touches[a].position.x, Input.touches[a].position.y, 13)), Vector2.zero);
+            if (hit.collider != null)
+            {
+                if (Input.touches[a].phase == TouchPhase.Began)
+                {
+                    if (hit.collider.name == "Up")
+                    {
+                        if (!jumping)
+                        {
+                            rigidbody.AddForce(Vector3.up * jumpHight);
+                            jumping = true;
+                        }
+                    }
+                }
+                if (Input.touches[a].phase == TouchPhase.Stationary || Input.touches[a].phase == TouchPhase.Moved)
+                {
+
+                    if (hit.collider.name == "Left")
+                    {
+                        transform.position = transform.position + Vector3.left * Time.deltaTime;
+                    }
+                    if (hit.collider.name == "Right")
+                    {
+                        transform.position = transform.position + Vector3.right * Time.deltaTime;
+                    }
+                    if (hit.collider.name == "Down")
+                    {
+
+                    }
+
+                }
+            }
+        }*/
     }
-    private void OnCollisionEnter(Collision other)
+    private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Platform")
+        if(other.tag =="Platform")
         {
             jumping = false;
         }
