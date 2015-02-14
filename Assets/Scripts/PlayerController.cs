bf968a3f1a2644bebfefbe6716a9ad7c92ae9e64
BeauArthurs <BeauArthurs@gmail.com>
2015-02-14 23:18:09 +0100

    Beau_007

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    [SerializeField]
+    private int speed;
+    private int pressure;
+    private int resistance;
+    [SerializeField]
+    private float air;
+    private int health;
+	void Start () 
+    {
+        health = 100;
+        resistance = -50;
+        air = 100;
+	}
+	void Update ()
+    {
+        pressure = Mathf.FloorToInt(transform.position.y) - resistance;
+        if(pressure < 0 )
+        {
+            Debug.Log("you are to low");
+        }
+        air -= 1 * Time.deltaTime;
+    }
+    public void Move(Vector3 direction)
+    {
+        transform.position = transform.position + (direction * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/controller.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class controller : MonoBehaviour {
+    private int fingerOnStick;
+    [SerializeField]
+    private GameObject joyStick;
+    public Vector3 JoyStickOutput;
+	void Start () 
+    {
+        fingerOnStick = 20;
+	}
+	
+	void Update () 
+    {
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            Touch touch = Input.touches[i];
+            Vector3 position = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 30));
+            RaycastHit2D hit = Physics2D.Raycast(position, Vector2.zero);
+            if(i == fingerOnStick)
+            {
+                if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)
+                {
+                    JoyStickOutput = joyStick.GetComponent<JoyStick>().Move(position);
+                }
+                else if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)
+                {
+                    fingerOnStick = 20;
+                    JoyStickOutput = Vector3.zero;
+                    joyStick.GetComponent<JoyStick>().Stop();
+                }
+            }
+            if (hit != null && hit.collider != null)
+            {
+                if (hit.collider.name == "JoyStick")
+                {
+                    if (touch.phase == TouchPhase.Began)
+                    {
+                        fingerOnStick = i;
+                    } 
+                }
+                else
+                {
+                    Debug.Log(hit.collider.name);
+                }
+            }
+        }
+	}
+}
