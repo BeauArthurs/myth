bf27b86a56999c32ebf46ce5d25d4381eb19213d
BeauArthurs <BeauArthurs@gmail.com>
2015-02-18 19:57:47 +0100

    Beau_010

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bomb : MonoBehaviour {
+
+    private float distance;
+	void Update () 
+    {
+	
+	}
+    void OnTriggerStay(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            distance = Vector3.Distance(transform.position, other.transform.position);
+            if(distance < 5)
+            {
+                other.GetComponent<PlayerOperator>().ChangeHealth(-20);
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerOperator.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerOperator : MonoBehaviour {
+    [SerializeField]
+    Slider _airSlider;
+    [SerializeField]
+    Slider _pressureSlider;
+    [SerializeField]
+    Slider _healthSlider;
+    [SerializeField]
+    private int _speed;
+    private int _pressure;
+    private int _resistance;
+    private float _air;
+    private int _health;
+    private bool underWater;
+	void Start () 
+    {
+        _health = 100;
+        _resistance = -50;
+        _air = 100;
+	}
+	void Update ()
+    {
+        _airSlider.value = _air;
+        _pressure = Mathf.FloorToInt(transform.position.y) - _resistance;
+        if(_pressure < 0 && _pressure > -100 )
+        {
+            _pressureSlider.value = -_pressure;
+        }
+        if(underWater)
+        {
+            _air -= 1 * Time.deltaTime;
+        }
+        else if(!underWater && _air < 100)
+        {
+            _air += 5 * Time.deltaTime;
+        }
+        
+    }
+    public void ChangeHealth(int hitPoints)
+    {
+        _health += hitPoints;
+        _healthSlider.value = _health;
+    }
+    public void Move(Vector3 direction)
+    {
+        rigidbody.AddForce(direction * _speed * Time.deltaTime);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Air")
+        {
+            underWater = false;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Air")
+        {
+            underWater = true;
+        }
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag =="Wall")
+        {
+            ChangeHealth(-5);
+        }
+    }
+}
