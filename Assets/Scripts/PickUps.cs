3aa57369510c887b63c866b5e6ca0a24ceafc0a0
kevinboogaard <kevin.boogaard3@gmail.com>
2015-03-04 09:41:16 +0100

    kevin 009
    pickups

---
+++ Assets/Scripts/PickUps.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUps : MonoBehaviour {
+    private PlayerOperator _operatorController;
+    public int typePickUp;
+	// Use this for initialization
+	void Start () {
+        _operatorController = GameObject.FindGameObjectWithTag("player").GetComponent<PlayerOperator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        foreach (Touch touch in Input.touches)
+        {
+            if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled)
+            {
+                if (typePickUp == 1)
+                {
+                    _operatorController.AddMoney(10);
+                    Debug.Log("add money 10");
+                }
+            }
+        }
+	}
+}
